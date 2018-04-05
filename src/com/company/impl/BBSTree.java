e557ca3e9c602602ec367b586fa6b8b02113060d
ala_ropat <ala_ropat@epam.com>
2018-04-05 17:45:04 +0300

    implement AVLTree

--- src/com/company/impl/BBSTree.java
+++ src/com/company/impl/BBSTree.java
@@ -3,8 +3,9 @@ package com.company.impl;
 import com.company.abstr.BSTree;
 
 public class BBSTree<E> implements BSTree<E> {
-private static int size;
-private TreeNode<E> root;
+
+  private static int size;
+  private Node <E> root;
   @Override
   public E max() {
     return null;
@@ -43,5 +44,70 @@ private TreeNode<E> root;
   @Override
   public void print() {
 
+  }
+
+  /**
+   * Height is calculated via the following formula
+   * height = max(leftSubTreeHeight,rightSubTreeHeight)+1
+   *
+   * @return -1 if the node is null. Otherwise, return the height
+   */
+
+  private int height(Node <E> currentNode) {
+    return (currentNode == null) ? -1 : currentNode.getHeight();
+  }
+
+  private class Node<E> {
+
+    private E data;
+    private Node <E> parent;
+    private Node <E> leftChild;
+    private Node <E> rightChild;
+    private int height;
+
+
+    public Node(E data) {
+      this.data = data;
+    }
+
+    public int getHeight() {
+      return height;
+    }
+
+    public void setHeight(int height) {
+      this.height = height;
+    }
+
+    public E getData() {
+      return data;
+    }
+
+    public void setData(E data) {
+      this.data = data;
+    }
+
+    public Node <E> getParent() {
+      return parent;
+    }
+
+    public void setParent(Node <E> parent) {
+      this.parent = parent;
+    }
+
+    public Node <E> getLeftChild() {
+      return leftChild;
+    }
+
+    public void setLeftChild(Node <E> leftChild) {
+      this.leftChild = leftChild;
+    }
+
+    public Node <E> getRightChild() {
+      return rightChild;
+    }
+
+    public void setRightChild(Node <E> rightChild) {
+      this.rightChild = rightChild;
+    }
   }
 }
